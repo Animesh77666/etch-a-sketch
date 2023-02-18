b321ef4e74c190b18467921be90a9a0d0f07e4f0
Animesh77666 <animesh77666@gmail.com>
2023-02-18 18:58:59 +0530

    temp

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
-  function boardsize() {
+  function boardsize(size= 32*44, s =  'medium-grid-default') {
     const sketchpad = document.getElementById("sketchpad");
-    size = 32 * 44
+    sketchpad.innerHTML = '';
 
-    sketchpad.classList.remove(
-      "small-grid",
-      "medium-grid-default",
-      "big-grid"
-    );
-    sketchpad.classList.add("medium-grid-default");
+    sketchpad.classList.remove("small-grid", "medium-grid-default", "large-grid");
+    sketchpad.classList.add(s);
+    console.log(sketchpad.classList)
 
     for (let i = 0; i < size; i += 1) {
-        const div = document.createElement('div');
-        sketchpad.appendChild(div);
-      }
+      const div = document.createElement("div");
+      sketchpad.appendChild(div);
+    }
   }
 
-  boardsize();
+  function getScreenSize() {
+    const ssize = document.getElementById("small");
+    const msize = document.getElementById("medium");
+    const lsize = document.getElementById("large");
+
+    ssize.addEventListener("click", () => boardsize(16 * 22,"small-grid"));
+    msize.addEventListener("click", () => boardsize(32 * 44,"medium-grid-default"));
+    lsize.addEventListener("click", () => boardsize(64 * 88,"large-grid"));
+  }
+
+
+  
+
+  function startGame() {
+    boardsize();
+    getScreenSize();
+    changeSize();
+  }
+
+  startGame();
 });
